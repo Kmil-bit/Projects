dbbd05acd5e9b882eefc3326a34227e9e826f83a
Kmil-bit <kamil.klak14@gmail.com>
2025-03-10 16:17:32 +0100

    Add files via upload

---
+++ labs2-4/lab2.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+
+namespace ConsoleApp3
+{
+	internal class Program
+	{
+		/*	static bool spalindrom(string palindrom)
+			{
+				if (palindrom=palindrom.Reverse)
+				{
+					return true;
+				}else
+				return false;
+			}*/
+		/*static bool isPalindrom(string txt)
+			{
+				for (int i = 0; i < txt.Length; i++)
+				{
+					if (txt[i] != txt[txt.Length - 1 - i])
+					{
+						return false;
+					}
+				}
+				return true;
+			}*/
+		static ArrayList czynnikiPierwsze(int liczba)
+			{
+				ArrayList lista = new ArrayList();
+				int dzielnik = 2;
+				while (liczba != 1)
+				{
+					if (liczba % dzielnik == 0)
+					{
+						liczba = liczba / dzielnik;
+						lista.Add(dzielnik);
+					}
+					else
+					{
+						dzielnik++;
+					}
+				}
+				return lista;
+			}
+		/*static void cochcesz()
+			{
+				Console.WriteLine("Podaj 1 liczbe");
+				string x = Console.ReadLine();
+				Console.WriteLine("Podaj 2 liczbe");
+				string y = Console.ReadLine();
+				Console.WriteLine("Co chcesz zrobic");
+				Console.WriteLine("1 - dodaj");
+				Console.WriteLine("2 - odejmij");
+				string decyzja = Console.ReadLine();
+
+				switch (decyzja)
+				{
+					case "1":
+						int wynik = int.Parse(x) + int.Parse(y);
+						Console.WriteLine("Wynik dodawania: " + wynik);
+						break;
+					case "2":
+						int wynik2 = int.Parse(x) - int.Parse(y);
+						Console.WriteLine("Wynik odejmowania: " + wynik2);
+						break;
+				}
+			}*/
+		/*static double convert(string number, int baseNumber)
+			{
+				double result = 0;
+
+				for(int i = 0; i < number.Length; i++)
+				{
+					int liczba = int.Parse(number[i].ToString());
+					int potega = number.Length - 1 - i;
+					result = result + liczba * Math.Pow(baseNumber, potega);
+				}
+				return result;
+			}*/
+
+		static void Main(string[] args)
+		{
+			//		Console.WriteLine(isPalindrom("kajak"));
+				ArrayList wynik = czynnikiPierwsze(22);
+				foreach (int i in wynik)
+				{
+					Console.WriteLine(i);
+				}
+			/*	Console.WriteLine("Podaj 1 liczbe");
+                string x = Console.ReadLine();
+                Console.WriteLine("Podaj 2 liczbe");
+                string y = Console.ReadLine();
+                Console.WriteLine("Co chcesz zrobic");
+                Console.WriteLine("1 - dodaj");
+                Console.WriteLine("2 - odejmij");
+                string decyzja = Console.ReadLine();
+
+                switch (decyzja)
+                {
+                    case "1":
+                        int wynik = int.Parse(x) + int.Parse(y);
+                        Console.WriteLine("Wynik dodawania: " + wynik);
+                        break;
+                    case "2":
+                        int wynik2 = int.Parse(x) - int.Parse(y);
+                        Console.WriteLine("Wynik odejmowania: " + wynik2);
+                        break;
+                }
+            */
+			//    Console.WriteLine(convert("1111",2));
+		}
+	}
+}
---
+++ labs2-4/lab3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	static void Main()
+	{
+		string fileName = "kody 1.txt"; // Ustaw nazwę pliku na odpowiednią
+
+		try
+		{
+			// Otwórz plik do odczytu
+			using (StreamReader sr = new StreamReader(fileName))
+			{
+				string line;
+
+				// Odczytaj każdą linię z pliku
+				while ((line = sr.ReadLine()) != null)
+				{
+					// Sprawdź, czy cała linia składa się z cyfr parzystych
+					if (IsAllDigitsEven(line))
+					{
+						Console.WriteLine(line);
+					}
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Wystąpił błąd: {e.Message}");
+		}
+		char lol = '5';
+		Console.Write(lol % 2);
+	}
+
+
+
+	// Funkcja sprawdzająca, czy wszystkie cyfry w ciągu są parzyste
+	static bool IsAllDigitsEven(string str)
+	{
+		foreach (char c in str)
+		{
+			if (!char.IsDigit(c) ||  c  % 2 != 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
