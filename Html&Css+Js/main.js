31d44f3f498336fd9d9dd11d46fe1744a64b1f48
Kmil-bit <kamil.klak14@gmail.com>
2025-03-10 16:26:39 +0100

    Add files via upload

---
+++ Html&Css+Js/main.js
@@ -0,0 +1,21 @@
+function KolorFunkcjaL() {
+   let button = document.getElementById('button');
+   let container = document.getElementById('main');
+   let container2 = document.getElementById('aside');
+   container2.style.background = "red";
+   container.style.background = "yellow";
+}
+function KolorFunkcjaD(){
+    let button = document.getElementById('button');
+   let container = document.getElementById('main');
+   let container2 = document.getElementById('aside');
+   container2.style.background = "purple";
+   container.style.background = "darkgreen";
+}
+function KolorFunkcjaN(){
+    let button = document.getElementById('button');
+   let container = document.getElementById('main');
+   let container2 = document.getElementById('aside');
+   container2.style.background = "orange";
+   container.style.background = "cornsilk";
+}
